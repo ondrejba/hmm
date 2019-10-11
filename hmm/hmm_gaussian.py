fix bugs and numerical problems

hmm/hmm_gaussian.py
@@ -15,6 +15,7 @@ class HMMGaussian:
         if init is not None and self.mu is not None:
             self.num_hidden_states = len(self.init)
             self.dimensionality = self.mu.shape[1]
+            self.enabled = np.array([True] * self.num_hidden_states)
 
     def learn_fully_obs(self, xs, zs):
 
@@ -22,6 +23,7 @@ class HMMGaussian:
         _, Nk = np.unique(zs, return_counts=True)
         self.num_hidden_states = len(np.unique(zs))
         self.dimensionality = xs.shape[2]
+        self.enabled = np.array([True] * self.num_hidden_states)
 
         Njl = np.zeros((self.num_hidden_states, self.num_hidden_states), dtype=np.float32)
 
@@ -52,7 +54,7 @@ class HMMGaussian:
         self.A = np.random.dirichlet([1.0] * self.num_hidden_states * self.num_hidden_states) \
             .reshape((self.num_hidden_states, self.num_hidden_states))
         self.mu = np.random.normal(0, 1, size=(self.num_hidden_states, self.dimensionality))
-        self.cov = np.tile(np.diag([0.1] * self.dimensionality)[np.newaxis, :, :],
+        self.cov = np.tile(np.diag([1.0] * self.dimensionality)[np.newaxis, :, :],
                            reps=(self.num_hidden_states, 1, 1))
         self.enabled = np.array([True] * self.num_hidden_states)
 
@@ -101,18 +103,15 @@ class HMMGaussian:
 
         self.cov = mean_xx_bar - self.mu[:, :, np.newaxis] * self.mu[:, np.newaxis, :]
 
-        self.enabled[:] = True
-        for i in range(self.num_hidden_states):
-            if not self.is_valid(self.cov[i]):
-                self.enabled[i] = False
+        self.enabled[log_Nj <= np.log(10.0)] = False
 
         # log likelihood
         log_px = self.condition(xs)
         log_px = np.transpose(log_px, axes=[1, 2, 0])
 
-        log_likelihood = np.sum(np.exp(log_N1) * np.log(self.init)) + \
-                         np.sum(np.exp(log_Njk) * np.log(self.A)[np.newaxis, np.newaxis, :, :]) + \
-                         np.sum(np.exp(log_gammas_batch) * log_px)
+        log_likelihood = np.sum((np.exp(log_N1) * log_init)[self.enabled]) + \
+                         np.sum((np.exp(log_Njk) * log_A)[self.enabled[:, np.newaxis] * self.enabled[np.newaxis, :]]) + \
+                         np.sum((np.exp(log_gammas_batch) * log_px)[:, :, self.enabled])
 
         return log_likelihood
 
@@ -208,4 +207,4 @@ class HMMGaussian:
         return alpha - s, s
 
     def is_valid(self, x):
-        return np.all(np.linalg.eigvals(x) >= 0) and np.linalg.matrix_rank(x) == x.shape[0]
+        return np.all(np.linalg.eigvals(x) >= 0) and np.linalg.matrix_rank(x) == x.shape[0] and not np.all(x == 0)

hmm/hmm_gaussian_tf.py
@@ -20,7 +20,7 @@ class HMMGaussianTF:
             np.random.normal(0, 1, size=(self.num_hidden_states, self.dimensionality)), trainable=True, dtype=tf.float32
         )
         self.cov = tf.Variable(
-            np.tile(np.diag([0.1] * self.dimensionality)[np.newaxis, :, :], reps=(self.num_hidden_states, 1, 1)),
+            np.tile(np.diag([1.0] * self.dimensionality)[np.newaxis, :, :], reps=(self.num_hidden_states, 1, 1)),
             trainable=True, dtype=tf.float32
         )
 
@@ -30,7 +30,7 @@ class HMMGaussianTF:
 
         self.seq = tf.placeholder(tf.float32, shape=(None, self.seq_length, self.dimensionality), name="seq_pl")
         self.batch_size = tf.shape(self.seq)[0]
-        self.condition_seq = self.condition(self.seq)
+        self.log_condition_seq = self.condition(self.seq)
 
     def setup(self):
 
@@ -51,61 +51,62 @@ class HMMGaussianTF:
 
         # log likelihood
         log_likelihood = tf.reduce_sum(N1 * tf.log(self.init)) + \
-            tf.reduce_sum(Njk * tf.log(self.A)) + tf.reduce_sum(gammas_batch * tf.log(self.condition_seq))
+            tf.reduce_sum(Njk * tf.log(self.A)) + tf.reduce_sum(gammas_batch * self.log_condition_seq)
 
         return log_likelihood
 
     def forward_backward(self):
 
-        alphas, log_evidence = self.forward()
-        log_alphas = tf.log(alphas)
+        log_alphas, log_evidence = self.forward()
         log_betas = self.backward()
-        betas = tf.exp(log_betas)
 
         log_gammas = log_alphas + log_betas
         log_gammas = log_gammas - tf.reduce_logsumexp(log_gammas, axis=1)[:, tf.newaxis]
         gammas = tf.exp(log_gammas)
 
         log_A = tf.log(self.A)
-        log_px = tf.log(self.condition_seq)
 
-        log_etas = log_A[tf.newaxis, tf.newaxis, :, :] + (log_px[:, 1:, :] + log_betas[:, 1:, :])[:, :, tf.newaxis, :] + log_alphas[:, :-1, :][:, :, :, tf.newaxis]
+        log_etas = log_A[tf.newaxis, tf.newaxis, :, :] + (self.log_condition_seq[:, 1:, :] +
+            log_betas[:, 1:, :])[:, :, tf.newaxis, :] + log_alphas[:, :-1, :][:, :, :, tf.newaxis]
         log_etas = log_etas - tf.reduce_logsumexp(log_etas, axis=(1, 2))[:, tf.newaxis, tf.newaxis]
         etas = tf.exp(log_etas)
 
-        return alphas, log_evidence, betas, gammas, etas
+        return log_alphas, log_evidence, log_betas, gammas, etas
 
     def forward(self):
 
-        alpha_1 = self.condition_seq[:, 0, :] * self.init[tf.newaxis, :]
-        alpha_1, Z_1 = self.normalize(alpha_1)
+        log_init = tf.log(self.init)
+        log_A = tf.log(self.A)
 
-        alphas = [alpha_1]
-        Zs = [Z_1]
+        log_alpha_1 = self.log_condition_seq[:, 0, :] + log_init[tf.newaxis, :]
+        log_alpha_1, log_Z_1 = self.log_normalize(log_alpha_1)
+
+        log_alphas = [log_alpha_1]
+        log_Zs = [log_Z_1]
 
         for t in range(1, self.seq_length):
 
-            alpha_t = self.condition_seq[:, t, :] * tf.matmul(
-                tf.transpose(self.A, perm=[1, 0])[tf.newaxis, :], alphas[-1][:, :, tf.newaxis]
-            )[:, :, 0]
+            log_alpha_t = self.log_condition_seq[:, t, :] + tf.reduce_logsumexp(
+                log_A[tf.newaxis, :, :] + log_alphas[-1][:, :, tf.newaxis], axis=1
+            )
 
-            alpha_t, Zt = self.normalize(alpha_t)
+            log_alpha_t, log_Zt = self.log_normalize(log_alpha_t)
 
-            alphas.append(alpha_t)
-            Zs.append(Zt)
+            log_alphas.append(log_alpha_t)
+            log_Zs.append(log_Zt)
 
-        alphas = tf.stack(alphas, axis=1)
-        Zs = tf.stack(Zs, axis=1)
+        log_alphas = tf.stack(log_alphas, axis=1)
+        log_Zs = tf.stack(log_Zs, axis=1)
 
-        log_evidence = tf.reduce_sum(tf.log(Zs), axis=1)
+        log_evidence = tf.reduce_sum(log_Zs, axis=1)
 
-        return alphas, log_evidence
+        return log_alphas, log_evidence
 
     def backward(self):
 
         log_betas = [tf.zeros((self.batch_size, self.num_hidden_states), dtype=tf.float32)]
         log_A = tf.log(self.A)
-        log_px = tf.log(self.condition_seq)
+        log_px = self.log_condition_seq
 
         for t in reversed(range(0, self.seq_length - 1)):
 
@@ -122,12 +123,12 @@ class HMMGaussianTF:
 
         seq = tf.reshape(seq, shape=(-1, self.dimensionality))
 
-        px = self.dists.prob(seq[:, tf.newaxis, :])
-        px = tf.reshape(px, shape=(-1, self.seq_length, self.num_hidden_states))
+        log_px = self.dists.log_prob(seq[:, tf.newaxis, :])
+        log_px = tf.reshape(log_px, shape=(-1, self.seq_length, self.num_hidden_states))
 
-        return px
+        return log_px
 
-    def normalize(self, alpha):
+    def log_normalize(self, alpha):
 
-        s = tf.reduce_sum(alpha, axis=-1)
-        return alpha / s[..., tf.newaxis], s
+        s = tf.reduce_logsumexp(alpha, axis=-1)
+        return alpha - s[..., tf.newaxis], s

hmm/scripts/embeddings_no_actions_learn.py
@@ -17,8 +17,6 @@ xs = np.reshape(xs, (-1, 20))
 xs = pca.fit_transform(xs)
 xs = np.reshape(xs, (-1, 2, 2))
 
-print(xs[0])
-
 # learn
 hmm = HMMGaussian()
 hmm.initialize_em(10, 2)
@@ -28,36 +26,19 @@ for i in range(50):
     # learn
     print("step", i)
     #print(hmm.A)
+    print(hmm.enabled)
     #print(hmm.init)
     #print(hmm.mu)
     #print(hmm.cov)
     #print()
 
-    ll = hmm.learn_em(xs[:100])
+
+    ll = hmm.learn_em(xs)
     print("log likelihood:", ll)
     print()
 
 # calculate probabilities
-log_alphas, log_evidence, log_betas, log_gammas, log_etas = hmm.forward_backward(xs[0])
-alphas, gammas = np.exp(log_alphas), np.exp(log_gammas)
-
-# plot alphas and gammas
-plot_zs = np.array(ys)
-plot_alphas = alphas[:, 1]
-plot_gammas = gammas[:, 1]
-plot_xs = np.linspace(1, len(plot_zs), num=len(plot_zs))
-
-plt.figure(figsize=(12, 9))
-
-plt.subplot(2, 1, 1)
-plt.title("filtering")
-plt.plot(plot_xs, plot_zs, label="z")
-plt.plot(plot_xs, plot_alphas, label="P(z) = 1")
-plt.legend()
-
-plt.subplot(2, 1, 2)
-plt.title("smoothing")
-plt.plot(plot_xs, plot_zs, label="z")
-plt.plot(plot_xs, plot_gammas, label="P(z) = 1")
-plt.legend()
+plot_xs = xs.reshape((-1, 2))
+plt.scatter(plot_xs[:, 0], plot_xs[:, 1])
+plt.scatter(hmm.mu[:, 0], hmm.mu[:, 1])
 plt.show()

hmm/tests/test_hmm_gaussian_tf.py
@@ -1,7 +1,14 @@
+import os
+import logging
 import unittest
+import numpy as np
 import tensorflow as tf
 from ..hmm_gaussian_tf import HMMGaussianTF
 from . import utils
+
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+tf.get_logger().setLevel(logging.ERROR)
 
 
 class TestHMMGaussianTF(unittest.TestCase):
@@ -24,6 +31,14 @@ class TestHMMGaussianTF(unittest.TestCase):
         self.assertEqual(utils.get_tensor_shape(alphas), (None, self.SEQ_LENGTH, self.NUM_HIDDEN_STATES))
         self.assertEqual(utils.get_tensor_shape(log_evidence), (None,))
 
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            alphas, log_evidence = sess.run([alphas, log_evidence], feed_dict={
+                model.seq: np.random.uniform(-1, 1, size=(13, 11, 7))
+            })
+            self.assertTrue(np.all(np.bitwise_not(np.isnan(alphas))))
+            self.assertTrue(np.all(np.bitwise_not(np.isnan(log_evidence))))
+
     def test_backward(self):
 
         model = HMMGaussianTF(self.NUM_HIDDEN_STATES, self.DIMENSIONALITY, self.SEQ_LENGTH)
@@ -32,6 +47,13 @@ class TestHMMGaussianTF(unittest.TestCase):
 
         log_betas = model.backward()
         self.assertEqual(utils.get_tensor_shape(log_betas), (None, self.SEQ_LENGTH, self.NUM_HIDDEN_STATES))
+
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            log_betas_val = sess.run(log_betas, feed_dict={
+                model.seq: np.random.uniform(-1, 1, size=(13, 11, 7))
+            })
+            self.assertTrue(np.all(np.bitwise_not(np.isnan(log_betas_val))))
 
     def test_forward_backward(self):
 
@@ -54,4 +76,5 @@ class TestHMMGaussianTF(unittest.TestCase):
         model.setup_variables()
         model.setup_placeholders()
 
-        print(model.likelihood())
+        log_likelihood = model.likelihood()
+        self.assertEqual(len(log_likelihood.shape), 0)