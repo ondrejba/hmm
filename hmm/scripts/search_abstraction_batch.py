9c41d1776374f3b937258b175427976015ab17ba
Ondrej Biza <ondrej.biza@gmail.com>
2019-10-16 11:53:59 -0400

    clean graph after run

--- hmm/scripts/search_abstraction_batch.py
+++ hmm/scripts/search_abstraction_batch.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import numpy as np
+import tensorflow as tf
 from ..runners import abstraction_learn_actions_tf
 
 lrs = [1.0, 0.5, 0.1, 0.05, 0.01, 0.005, 0.001]
@@ -16,7 +17,8 @@ if not os.path.isdir("./results"):
     os.makedirs("./results")
 
 if os.path.isfile(results_path):
-    results = pickle.load(results_path)
+    with open(results_path, "rb") as file:
+        results = pickle.load(file)
 
 for i, mu_sd in enumerate(mu_sds):
     for j, cov_sd in enumerate(cov_sds):
@@ -27,9 +29,12 @@ for i, mu_sd in enumerate(mu_sds):
                     if (mu_sd, cov_sd, lr, dim, run_idx) in results:
                         continue
 
+                    print("running:", (mu_sd, cov_sd, lr, dim, run_idx))
+
                     best_accuracy, _ = abstraction_learn_actions_tf.main(
                         dim, 10, lr, 500, 100, False, 100, mu_sd, cov_sd, False, "1"
                     )
+                    tf.reset_default_graph()
 
                     if np.any(np.isnan(best_accuracy)):
                         best_accuracy = 0.0
--- hmm/scripts/search_abstraction_batch_2.py
+++ hmm/scripts/search_abstraction_batch_2.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import numpy as np
+import tensorflow as tf
 from ..runners import abstraction_learn_actions_tf
 
 lrs = [1.0, 0.5, 0.1, 0.05, 0.01, 0.005, 0.001]
@@ -28,6 +29,7 @@ for i, num_hidden_states in enumerate(num_hidden_states_list):
                 best_accuracy, _ = abstraction_learn_actions_tf.main(
                     dim, num_hidden_states, lr, 500, 100, False, 100, 1.0, 1.0, False, "0"
                 )
+                tf.reset_default_graph()
 
                 if np.any(np.isnan(best_accuracy)):
                     best_accuracy = 0.0
